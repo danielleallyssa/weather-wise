3a63cb44ece50a92516c17ffe8ac6fddf2e38482
Danielle Brown <hello@danielleallyssa.com>
2024-04-30 00:03:56 -0400

    Got the API url working and set up some DOM elements

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const url = "http://dataservice.accuweather.com/currentconditions/v1/";
+let locationKey = "";
 
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
+document.querySelector("button").addEventListener("click", getLocation);
+function getLocation() {
+  let userInput = document.querySelector("input").value;
+  const locationUrl = `https://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=oRTBi9VpaBqXfJaUo4rD1C9YVxChJyAx&q=${userInput}`;
+
+  fetch(locationUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let city = data[0].EnglishName;
+      let state = data[0].AdministrativeArea.EnglishName;
+      console.log(city, state);
+      getWeather(data[0].Key);
+      document.querySelector("h2").textContent = city + ", " + state;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+function getWeather(location) {
+  const weatherUrl = `https://dataservice.accuweather.com/currentconditions/v1/${location}?apikey=oRTBi9VpaBqXfJaUo4rD1C9YVxChJyAx`;
+  fetch(weatherUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data[0].WeatherText);
+      document.querySelector("p").textContent = data[0].WeatherText;
+    });
+}
+
+function swapDOMcontent(obj) {}
